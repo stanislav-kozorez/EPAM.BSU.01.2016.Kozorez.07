1af4133797970399a8a5e1171adaabf98de73640
_shark <stanislav.kozorez@gmail.com>
2016-03-31 16:17:01 +0300

    Task 5 was done with unit tests

---
+++ Logic/Task5/Sort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Task5
+{
+    public static class Sort
+    {
+        public static void BubbleSort<T>(IList<T> arr, IComparer<T> comparer = null)
+        {
+            if (arr == null)
+                throw new ArgumentNullException($"{nameof(arr)}");
+            IComparer<T> c = comparer ?? Comparer<T>.Default;
+            BubbleSortImpl(arr, c);
+        }
+
+        private static void BubbleSortImpl<T>(IList<T> arr, IComparer<T> comparer)
+        {
+            for (int i = 0; i < arr.Count() - 1; i++)
+                for (int j = 0; j < arr.Count() - i - 1; j++)
+                    if (comparer.Compare(arr[j], arr[j + 1]) > 0)
+                        Swap(arr, j);
+        }
+
+        private static void Swap<T>(IList<T> arr, int index)
+        {
+            T temp = arr[index];
+            arr[index] = arr[index + 1];
+            arr[index + 1] = temp;
+        }
+    }
+}
---
+++ NUnitTests/Task5/SortTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Logic.Task5;
+
+namespace NUnitTests.Task5
+{
+    [TestFixture]
+    public class SortTests
+    {
+        [Test]
+        public void BubbleSortIntTest()
+        {
+            int[] initialIntArr = { 2, 7, -23, 5, 79, 5 };
+            int[] expectedIntArr = { -23, 2, 5, 5, 7, 79 };
+
+            Sort.BubbleSort(initialIntArr);
+            CollectionAssert.AreEqual(expectedIntArr, initialIntArr);
+        }
+
+        [Test]
+        public void BubbleSortStringTest()
+        {
+            string[] initialStringArr = { "aqqqq", "bcd", "032", "wer", "sdf", "bac" };
+            string[] expectedStringArr = { "032", "aqqqq", "bac", "bcd", "sdf", "wer" };
+
+            Sort.BubbleSort(initialStringArr);
+            CollectionAssert.AreEqual(expectedStringArr, initialStringArr);
+        }
+
+        [Test]
+        public void BubbleSortFirstClassTest()
+        {
+            TestClass[] initialTestClassArr = { new TestClass(4), new TestClass(-34), new TestClass(2) };
+            TestClass[] expectedTestClassArr = { new TestClass(-34), new TestClass(2), new TestClass(4) };
+
+            Sort.BubbleSort(initialTestClassArr, new Comparator());
+            CollectionAssert.AreEqual(expectedTestClassArr, initialTestClassArr);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentException))]
+        public void BubbleSortFirstClassReturnsArgumentExceptionTest()
+        {
+            TestClass[] initialTestClassArr = { new TestClass(4), new TestClass(-34), new TestClass(2) };
+            TestClass[] expectedTestClassArr = { new TestClass(-34), new TestClass(2), new TestClass(4) };
+          
+            Sort.BubbleSort(initialTestClassArr);
+            CollectionAssert.AreEqual(expectedTestClassArr, initialTestClassArr);
+        }
+
+    }
+
+    public class TestClass: IEquatable<TestClass>
+    {
+        private int value;
+
+        public int Value { get { return value; } }
+
+        public TestClass(int value)
+        {
+            this.value = value;
+        }
+
+        public int GetHashCode(TestClass obj)
+        {
+            return obj.value.GetHashCode();
+        }
+
+        public bool Equals(TestClass other)
+        {
+            if (other == null)
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            if (value == other.value)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public class Comparator : IComparer<TestClass>
+    {
+        public int Compare(TestClass x, TestClass y)
+        {
+            if (x.Value > y.Value)
+                return 1;
+            else if (x.Value < y.Value)
+                return -1;
+            else return 0;
+        }
+    }
+}
